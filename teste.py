eb63e82a9665228e62c7b516d58123479e4d59f7
Davidmtp58 <dmtppb@gmail.com>
2026-05-15 15:16:54 -0300

    Adiciona cálculo de saldo do mês com filtros de receitas e despesas (utilização do head, tail, info, describe), ajuste do data texto para data

--- teste.py
+++ teste.py
@@ -1,5 +1,47 @@
 import pandas as pd
 
-df = pd.read_csv("extrato_exemplo.csv")
+# Lê o CSV tratando a coluna 'data' como data real
+df = pd.read_csv("extrato_exemplo.csv", parse_dates=["data"])
 
-print(df)
+'''# Visão geral
+print("--- INFORMAÇÕES GERAIS ---")
+df.info()
+
+print()
+print("--- ESTATÍSTICAS ---")
+print(df.describe().round(2))
+
+print()
+print("--- PRIMEIRAS TRANSAÇÕES ---")
+print(df.head())
+
+print()
+print("--- ÚLTIMAS TRANSAÇÕES ---")
+print(df.tail())'''
+
+# Filtrar apenas despesas (valores negativos)
+print()
+despesas = df[df["valor"] < 0]
+
+print("--- DESPESAS DO MÊS ---")
+print(despesas)
+print()
+
+print("--- TOTAL DE DESPESAS ---")
+total_despesas = despesas["valor"].sum()
+print(total_despesas)
+
+print()
+receitas = df[df["valor"] > 0]
+
+# Filtrar entrada de receita
+print("--- TOTAL DE RECEITAS ---")
+print(receitas)
+total_receitas = receitas["valor"].sum()
+print(total_receitas)
+
+# Saldo 
+print()
+print("--- SALDO DO MÊS ---")
+saldo = total_receitas + total_despesas
+print(saldo)
